2fac44541d53366f4bfdd0588a1d5d7efa2bc4c1
monajafi <monajafi@gmail.com>
2019-08-11 16:02:54 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+void main() => runApp(MyApp(
+  post: fetchPost(),
+));
+
+Future<Post> fetchPost() async{
+  final response = 
+      await http.get("https://jsonplaceholder.typicode.com/posts/1");
+  if(response.statusCode == 200){
+    return Post.fromJson(json.decode(response.body));
+  }else{
+    throw Exception('Failed to load post');
+  }
+}
+class Post {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  Post({this.id, this.userId, this.title, this.body});
+
+  factory Post.fromJson(Map<String,dynamic> json){
+    return Post(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+      body: json['body']
+    );
+  }
+}
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  const MyApp({Key key, this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      home: Scaffold(
+        appBar: AppBar(title: Text("Fetch Data Example"),),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context,snapshot){
+              if(snapshot.hasData){
+                return Text(snapshot.data.title);
+              }else if(snapshot.hasError){
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
